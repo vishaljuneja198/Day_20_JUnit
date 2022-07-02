813990b5c7d017532554689e3087e39dce29f4e2
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-07-02 16:36:13 +0530

    Welcome message entry of junit testing

---
+++ src/main/java/com/bridgelabz/RegexFirstName.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class RegexFirstName {
+
+    RegexFirstName(){
+        System.out.println("Welcome to JUnit Testing");
+    }
+}
---
+++ src/test/java/com/bridgelabz/TestRegexFirstName.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Test;
+
+public class TestRegexFirstName {
+
+    @Test
+    public void welcome() {
+        RegexFirstName firstName = new RegexFirstName();
+       // boolean result = firstName.checkName("Rob");
+        //assertTrue(result);
+    }
+
+}
